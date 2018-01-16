28d730719dd4b380ab615c86c3e761b30c4a5559
Chris Hamm <chamm@misterhamm.com>
2018-01-16 16:00:55 -0500

    portfolio comments

--- js/portfolioJavaScript.js
+++ js/portfolioJavaScript.js
@@ -39,7 +39,7 @@ SECTON - ABOUT
 
 document.getElementById('profile-pic-lg-2').addEventListener('mouseover', superman);
 document.getElementById('profile-pic-lg-2').addEventListener('mouseout', steve);
-
+// NOTE: This is fun to do with js but you can acheive the same thing with pure CSS without the performance hit.  Try it out!
 
 
 
@@ -47,10 +47,31 @@ document.getElementById('profile-pic-lg-2').addEventListener('mouseout', steve);
 PROGESS BARS
 **********************************/
 
+// NOTE: If you're repeating yourself this much there is probably a better way to do it.  Look up DRY programming.  
+// In this case you're just changing the index and the width basically  try something like this. (hint, this can be greatly improved as well :) )
+
+/*
+const skillsbar = document.getElementById('skillsBars');
+
+showSkills = function(index, percentage) {
+  skillsbar.addEventListener('mouseover', () => {
+    progressbar[index].style.width = percentage + "%";
+    progressbar[index].innerHTML = percentage + "%";
+  });
+}
+
+showSkills(0, 80);
+showSkills(1, 75);
+showSkills(2, 60);
+showSkills(3, 90);
+showSkills(4, 90);
+showSkills(5, 90);
+*/
+
 for (i = 0; i < progressbar.length; i += 1) {
   skillsBars.addEventListener('mouseover', () => {
-    progressbar[0].style.width = "80%";
-    progressbar[0].innerHTML = "80%";
+    progressbar[i].style.width = "80%";
+    progressbar[i].innerHTML = "80%";
     progressbar[1].style.width = "75%";
     progressbar[1].innerHTML = "75%";
     progressbar[2].style.width = "60%";
